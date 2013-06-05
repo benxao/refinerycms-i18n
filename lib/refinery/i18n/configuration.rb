bba345c95e8bec936adf1b79bbfa86695862d977
benxao <benxaooi@gmail.com>
2013-06-05 10:44:37 +0700

    add support for i18n domains

--- lib/refinery/i18n/configuration.rb
+++ lib/refinery/i18n/configuration.rb
@@ -3,7 +3,7 @@ module Refinery
     include ActiveSupport::Configurable
 
     config_accessor :current_locale, :default_locale, :default_frontend_locale,
-                    :enabled, :frontend_locales, :locales
+                    :enabled, :frontend_locales, :locales, :domain_name_enabled, :domains_locales
 
     self.enabled = true
     self.default_locale = :en
@@ -11,5 +11,7 @@ module Refinery
     self.current_locale = self.default_locale
     self.frontend_locales = [self.default_frontend_locale]
     self.locales = self.built_in_locales
+    self.domain_name_enabled = false
+    self.domains_locales = {"www.mysite.com"=>:en, "www.mon-site.fr"=>:fr}
   end
 end
